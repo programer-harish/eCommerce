5de0982296f6b56562914cbc83b3d3340d101ede
Harish <ht8679667@gmail.com>
2023-04-18 09:01:40 +0530

    Blank project with Backend only

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+
 
 function App() {
-  //const products=getProducts();
   return (
     <div>
-      <Header/>
-       <Home/>
-      <Footer/>
     </div>
   );
 }
